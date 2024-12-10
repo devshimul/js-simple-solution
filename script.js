08bcf84f488f7b8bbd8902228996419ff3354b24
devshimul <shimul.bisd@gmail.com>
2024-12-10 12:54:01 +0600

    Multi layer calculation

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log(125);
+/**
+ * First100Price = 100
+ * Second100Price = 90
+ * Above200Price = 70
+ */
+
+function layeredQuantityPrice(quantity) {
+  const first100Price = 100;
+  const second100Price = 90;
+  const above200Price = 70;
+
+  if (quantity <= 100) {
+    const totalPrice = quantity * first100Price;
+    return totalPrice;
+  } else if (quantity <= 200) {
+    const first100Total = 100 * first100Price;
+    const remainingQuantity = quantity - 100;
+    const remainingQuantityTotal = remainingQuantity * second100Price;
+    const total = first100Total + remainingQuantityTotal;
+    return total;
+  } else {
+    const first100Total = 100 * first100Price;
+    const second100Total = 100 * second100Price;
+    const remainingQuantity = quantity - 200;
+    const remainingQuantityTotal = remainingQuantity * above200Price;
+    const total = first100Total + second100Total + remainingQuantityTotal;
+    return total;
+  }
+}
+
+const totalPrice = layeredQuantityPrice(201);
+console.log(totalPrice);
